c3e0e0d87768d74c82e847e225c0919dc1d921a4
Ad450 <adjeimanue@gmail.com>
2022-07-05 16:29:41 +0000

    swap tokens page initilal phase done

---
+++ client/src/features/swap_tokens_page.tsx
@@ -0,0 +1,99 @@
+import {
+  Badge,
+  Box,
+  Button,
+  Center,
+  Collapse,
+  Divider,
+  Flex,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Spacer,
+  Stack,
+  Tag,
+  useDisclosure,
+} from "@chakra-ui/react";
+import * as React from "react";
+import {
+  BeeswapNormalText,
+  BeeswapSmallText,
+} from "../core/Beeswap_components/Text";
+import { BeeswapCollapseExProps } from "../core/interfaces/Beeswap_interfaces";
+import { BeeswapSvg } from "./connect_wallet_page";
+
+export const SwapPage = () => {
+  return (
+    <Center h="40%">
+      <Stack direction="column">
+        <Center>
+          <BeeswapSvg height="300" width="300"></BeeswapSvg>
+        </Center>
+        <Box
+          p={5}
+          shadow="lg"
+          border="1px"
+          borderColor="gray.400"
+          width="lg"
+          borderRadius="md"
+        >
+          <Flex direction="row">
+            <Box>
+              <Tag>ETH</Tag>
+              <Box height="20px"></Box>
+              <Stack direction="column">
+                <Box>
+                  <NumberInput maxW="150px">
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </Box>
+              </Stack>
+            </Box>
+            <Spacer></Spacer>
+            <Box>
+              <Tag>DAI</Tag>
+              <Box height="20px"></Box>
+              <BeeswapSmallText text="2333"></BeeswapSmallText>
+            </Box>
+          </Flex>
+        </Box>
+        <CollapseEx text="Confirm action"></CollapseEx>
+      </Stack>
+    </Center>
+  );
+};
+
+const CollapseEx = (beeswapCollapseExProps: BeeswapCollapseExProps) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onToggle} bg="teal">
+        Swap Tokens
+      </Button>
+      <Collapse in={isOpen} animateOpacity>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        >
+          {beeswapCollapseExProps.text}
+
+          <Button color="teal" marginLeft="40">
+            Confirm swap
+          </Button>
+        </Box>
+      </Collapse>
+    </>
+  );
+};
