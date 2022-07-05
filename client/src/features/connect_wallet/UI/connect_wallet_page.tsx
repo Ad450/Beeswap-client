80d37eb22f24255809430f23482103aa5ab2857c
Ad450 <adjeimanue@gmail.com>
2022-07-05 17:00:56 +0000

    moved files into respective directories

---
+++ client/src/features/connect_wallet/UI/connect_wallet_page.tsx
@@ -0,0 +1,117 @@
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  Spacer,
+  Stack,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { useState } from "react";
+import {
+  BeeswapNormalText,
+  BeeswapSmallText,
+  BeeswapText,
+} from "../../../core/Beeswap_components/Text";
+import {
+  BeeswapHeaderComponentProps,
+  BeeswapMiddleComponentProps,
+  BeeswapSvgProps,
+} from "../../../core/interfaces/Beeswap_interfaces";
+import { SwapPage } from "../../swap_tokens/UI/swap_tokens_page";
+
+export const ConnectWallet = () => {
+  // set to false for developing swap page
+  const [isConnectWallet, setConnectWallet] = useState(false);
+
+  const onConnectWalletClicked = () => {
+    setConnectWallet(!isConnectWallet);
+  };
+
+  return (
+    <Box
+      bg="gray.900"
+      w="100%"
+      h="container.xl"
+      p={4}
+      color="white"
+      padding={5}
+    >
+      <HeaderComponent
+        isConnectWallet={isConnectWallet}
+        isConnectWalletChanged={() => setConnectWallet(!isConnectWallet)}
+      ></HeaderComponent>
+
+      {isConnectWallet ? (
+        <SwapPage></SwapPage>
+      ) : (
+        <MiddleComponent
+          onConnectWalletClicked={() => setConnectWallet(!isConnectWallet)}
+        ></MiddleComponent>
+      )}
+    </Box>
+  );
+};
+
+const HeaderComponent = (
+  beeswapHeaderComponentProps: BeeswapHeaderComponentProps
+) => {
+  return (
+    <Flex minWidth="max-content" alignItems="center" gap="2">
+      <Box>
+        <BeeswapText text="Beeswap"></BeeswapText>
+      </Box>
+      <Spacer />
+      <Box>
+        {beeswapHeaderComponentProps.isConnectWallet ?(
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            onClick={() => beeswapHeaderComponentProps.isConnectWalletChanged()}
+          >
+            back
+          </Button>
+        ) : (
+          <BeeswapSmallText text="stay in touch"></BeeswapSmallText>
+        ) }
+      </Box>
+    </Flex>
+  );
+};
+
+const MiddleComponent = (
+  beeswapMiddleComponentProps: BeeswapMiddleComponentProps
+) => {
+  return (
+    <Center h="60%">
+      <Stack direction="column">
+        <Box p={5} shadow="md">
+          <BeeswapSvg height="300" width="300"></BeeswapSvg>
+          <BeeswapNormalText text="Connect wallet to continue using Beeswap"></BeeswapNormalText>
+        </Box>
+        <Button
+          colorScheme="teal"
+          variant="outline"
+          onClick={() => beeswapMiddleComponentProps.onConnectWalletClicked()}
+        >
+          <BeeswapNormalText text="Connect Metamask Wallet"></BeeswapNormalText>
+        </Button>
+      </Stack>
+    </Center>
+  );
+};
+
+export const BeeswapSvg = (beeswapSvgProps: BeeswapSvgProps) => {
+  return (
+    <object
+      data="https://svgsilh.com/svg/896670.svg"
+      width={beeswapSvgProps.width}
+      height={beeswapSvgProps.height}
+    >
+      {" "}
+    </object>
+  );
+};
+
+// simple onClick to toggle pages
