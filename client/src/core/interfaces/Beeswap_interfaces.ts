3c557a12aabf3ed05ae3aaadbea9cfca1774c5d7
Ad450 <adjeimanue@gmail.com>
2022-07-05 16:27:53 +0000

    props interfaces

---
+++ client/src/core/interfaces/Beeswap_interfaces.ts
@@ -0,0 +1,28 @@
+export interface BeeswapTextProps {
+    text: string;
+}
+export interface BeeswapNormalTextProps {
+    text: string;
+}
+
+export interface BeeswapSmallTextProps {
+    text: string;
+}
+
+export interface BeeswapSvgProps {
+    width: string;
+    height: string;
+}
+
+export interface BeeswapCollapseExProps {
+    text: string;
+}
+
+export interface BeeswapMiddleComponentProps {
+    onConnectWalletClicked: Function;
+}
+
+export interface BeeswapHeaderComponentProps {
+    isConnectWallet: boolean;
+    isConnectWalletChanged: Function;
+}
