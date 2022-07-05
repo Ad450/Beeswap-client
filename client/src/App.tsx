3e870e52f0c51ddbdc6f31559bcfdb4a35cc1bc4
Ad450 <adjeimanue@gmail.com>
2022-07-05 16:27:13 +0000

    Connect wallet page added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ConnectWallet } from "./features/connect_wallet_page";
 
-export const App = () => <ChakraProvider theme={theme}></ChakraProvider>;
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <ConnectWallet></ConnectWallet>
+  </ChakraProvider>
+);
