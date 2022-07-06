2ad82bfc6b3d417e4a49a925f253b43bb008cb5e
Ad450 <adjeimanue@gmail.com>
2022-07-06 23:07:19 +0000

    minor changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { ConnectWallet } from "./features/connect_wallet_page";
+import { ConnectWallet } from "./features/connect_wallet/UI/connect_wallet_page";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
